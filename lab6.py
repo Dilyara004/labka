9831d1830a50887a3f6f908a64b53111de557e61
unknown <shaimerdenn2004@gmail.com>
2024-04-24 13:28:57 +0500

    Add lab5

--- lab6.py
+++ lab6.py
@@ -25,3 +25,25 @@ decrement_button = tk.Button(root, text="Уменьшить", command=decrement_
 decrement_button.pack(side=tk.RIGHT, padx=10)
 
 root.mainloop()
+
+def lazy_prime_generator():
+    primes = []  # список для хранения простых чисел
+    num = 2  # начинаем с первого простого числа
+
+    while True:
+        is_prime = True
+        for prime in primes:
+            if num % prime == 0:  # проверяем, делится ли число на какое-либо из простых чисел
+                is_prime = False
+                break
+
+        if is_prime:
+            yield num  # если число простое, возвращаем его через генератор
+            primes.append(num)  # добавляем простое число в список простых чисел
+
+        num += 1  # переходим к следующему числу для проверки
+
+# Пример использования:
+prime_gen = lazy_prime_generator()
+for _ in range(10):
+    print(next(prime_gen))
